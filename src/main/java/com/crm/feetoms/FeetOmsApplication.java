ed12a9cd8991dab93c2f2c7e7ccc55f659934bae
方洋 <18069898338@163.com>
2021-07-09 10:30:55 +0800

    first commit

---
+++ src/main/java/com/crm/feetoms/FeetOmsApplication.java
@@ -0,0 +1,13 @@
+package com.crm.feetoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeetOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeetOmsApplication.class, args);
+    }
+
+}

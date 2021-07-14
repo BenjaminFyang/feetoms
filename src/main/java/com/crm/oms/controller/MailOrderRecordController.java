dc89f674fa26a9fd6031ded51ffaadafb43fafd7
方洋 <18069898338@163.com>
2021-07-14 16:01:55 +0800

    邮件订单操作日志

--- src/main/java/com/crm/oms/controller/MailOrderRecordController.java
+++ src/main/java/com/crm/oms/controller/MailOrderRecordController.java
@@ -1,9 +1,21 @@
 package com.crm.oms.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.crm.oms.common.api.CommonPage;
+import com.crm.oms.common.api.CommonResult;
+import com.crm.oms.dto.MailOrderParam;
+import com.crm.oms.model.MailOrder;
+import com.crm.oms.model.MailOrderRecord;
+import com.crm.oms.service.MailOrderRecordService;
+import com.crm.oms.service.MailOrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * 邮件订单操作记录信息表 前端控制器
@@ -11,9 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author fangyang
  * @since 2021-07-12
  */
+
+@Api(tags = "邮件订单操作日志")
 @RestController
 @RequestMapping("/mailOrderRecord")
 public class MailOrderRecordController {
+
+    @Resource
+    private MailOrderRecordService mailOrderRecordService;
+
+    @ApiOperation(value = "1、邮件订单操作日志列表")
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    public CommonResult<CommonPage<MailOrderRecord>> list() {
+        List<MailOrderRecord> mailOrderList = mailOrderRecordService.list();
+        return CommonResult.success(CommonPage.restPage(mailOrderList));
+    }
 
 }
 
